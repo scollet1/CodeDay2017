8eaebc611c30fc2ed4678820d942ff762a966a33
Sam Collet <scollet@student.42.us.org>
2017-05-20 19:43:37 -0700

    Update libs.js

--- js/libs.js
+++ js/libs.js
@@ -298,23 +298,6 @@ function ImprovedNoise () {
   }
 }
 
-AFRAME.registerComponent('color-on-beat', {
-  schema: {
-    analyserEl: {type: 'selector'}
-  },
-
-  init: function () {
-    var analyserEl = this.data.analyserEl || this.el;
-    var el = this.el;
-
-    analyserEl.addEventListener('audioanalyser-beat', function () {
-      el.setAttribute('material', 'color', '#' + new THREE.Color(
-        Math.random(), Math.random(), Math.random()
-      ).getHexString());
-    });
-  }
-});
-
 AFRAME.registerComponent('remove-on-event', {
   schema: {
     el: {type: 'selector'},
